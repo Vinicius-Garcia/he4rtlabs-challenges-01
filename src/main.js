77e9b8789ed49a6874fa23c4eeb7fdcee74d3a95
Vinicius Garcia <viniciusgarcia130@outlook.com>
2020-02-17 22:41:21 -0300

    calculo e começo do css

--- src/main.js
+++ src/main.js
@@ -2,5 +2,22 @@
 
 
 function calculate(){
+    var valorTotal = document.getElementById("valorTotal").value
+    var horas = document.getElementById("horas").value
+    var diasE = document.getElementById("diasE").value
+    var diasF = document.getElementById("diasF").value
+    
+    
+
+    
+
+    var valorHora = (valorTotal / (diasE * 4 * horas) ) + ( ( diasF * diasE * horas ) )
+   
+    
+    console.log(valorHora)
+    document.getElementById("result").innerHTML =+valorHora;
+    
+    
+
 
 }
