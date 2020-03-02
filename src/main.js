340591951caed524a1b4ddd6915a5a6805137f23
Vinicius Garcia <viniciusgarcia130@outlook.com>
2020-03-02 10:22:24 -0300

    visual update

--- src/main.js
+++ src/main.js
@@ -1,32 +1,24 @@
-
-
-
-function calculate(){
+function calculate() {
     var valorTotal = document.getElementById("valorTotal").value
     var horas = document.getElementById("horas").value
     var diasE = document.getElementById("diasE").value
     var diasF = document.getElementById("diasF").value
     var x = document.getElementById("resultado");
 
-    
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "block";
-    }
-    
-    
 
-    
 
-    var valorHora = (valorTotal / (diasE * 4 * horas) ) + ( ( diasF * diasE * horas ) )
-   
-    
-    console.log(valorHora)
-    document.getElementById("result").innerHTML =+valorHora;
-    
-    
+
+
+    var valorHora = (valorTotal / (diasE * 4 * horas)) + ((diasF * diasE * horas))
+
+
+
+    document.getElementById("result").innerHTML = "Valor total da hora é: R$:" + valorHora;
+
+
+
+
+
 
 
 }
-
